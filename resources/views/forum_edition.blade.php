a6c57c3025098bceb60308b85fe7d90a97b7b7bb
Nyxouu <justineperso03@gmail.com>
2024-12-04 16:17:40 +0100

    front-page-des-exposants + reorganisation-css

--- resources/views/forum_edition.blade.php
+++ resources/views/forum_edition.blade.php
@@ -33,9 +33,6 @@
                                     <p class="p-1 px-4 min-w-40 text-center">filiere de l'universite {{$k}}</p>
                                 @endfor
                             </div>
-                            <p class="pt-4">
-                                Lorem Ipsum est simplement du faux texte employé dans  la composition et la mise en page avant impression. Le Lorem Ipsum est  le faux texte standard de l'imprimerie depuis les années 1500, quand un  imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser  un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans  que son contenu n'en soit modifié.
-                            </p>
                         </div>
                     </div>
                 @endfor
