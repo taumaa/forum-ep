d8cbada6b198777c144cef27fd8aef448d7c7769
taumaa <youremail@example.com>
2024-12-11 10:37:42 +0100

    verify email page

---
+++ resources/views/verify-email.blade.php
@@ -0,0 +1,14 @@
+
+@extends('layouts.main')
+
+@section('title', 'Admin') <!-- Définir un titre spécifique -->
+
+@section('content')
+    <div class="w-full py-48 flex justify-center items-center">
+        <h1 class="text-3xl font-bold">Un email de vérification a été envoyé à votre adresse.</h1>
+    </div>
+@endsection
+
+
+
+
