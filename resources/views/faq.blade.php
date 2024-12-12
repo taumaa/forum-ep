f471269940bd7621d92d9d5023af414b195384ce
anne passelegue <annepas69@gmail.com>
2024-12-12 16:53:47 +0100

    Padding FAQ

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -8,8 +8,8 @@
         @foreach ($faqs as $faq) 
             <div class="flex flex-col my-1">
                 <details>
-                    <summary class="gray py-1 px-3 text-2xl cursor-pointer">{{ $faq->question }}</summary>
-                    <p class="white mx-9 border-l-2 border-dotted pl-2">{{ $faq->answer }}</p>
+                    <summary class="gray py-4 px-10 text-2xl cursor-pointer">{{ $faq->question }}</summary>
+                    <p class="white pl-4 py-3 mx-9 border-l-2 border-dotted pl-2">{{ $faq->answer }}</p>
                 </details>               
             </div>
         @endforeach
