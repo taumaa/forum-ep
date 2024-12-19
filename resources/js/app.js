cddf1fe33701bd8a626fd397eec7fe9fad3f32db
Nyxouu <justineperso03@gmail.com>
2024-12-19 12:29:09 +0100

    responsive-home-nav-footer

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,8 +9,6 @@ Alpine.start();
 
 // Récupérez l'élément du menu
 const nav = document.getElementById("nav");
-
-// Ajoutez un gestionnaire d'événements pour le défilement
 window.addEventListener("scroll", () => {
   if (window.scrollY > 100) { // Si le défilement dépasse 50px
     nav.classList.remove("nav-container"); // Retirer la classe par défaut
@@ -20,3 +18,14 @@ window.addEventListener("scroll", () => {
     nav.classList.add("nav-container"); // Ajouter la classe par défaut
   }
 });
+
+//ouverture et fermeture du menu burger
+const burgermenu = document.getElementById("burger-menu");
+const burgerbutton = document.getElementById("burger-button");
+let classes = burgermenu.classList;
+burgerbutton.addEventListener("click", function(){
+  classes.toggle("burger-menu-open");
+  classes.toggle("burger-menu-close");
+});
+
+
